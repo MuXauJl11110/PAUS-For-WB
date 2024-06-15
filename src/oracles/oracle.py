a081a09fda5c3d163ef43a1fb30262945363a8eb
Michael Persiianov <persiianov.mi@phystech.edu>
2024-06-15 14:48:42 +0300

    Add l2 regularization. Didn't help

--- src/oracles/oracle.py
+++ src/oracles/oracle.py
@@ -4,7 +4,7 @@ from src.oracles.point import BaseOracle, Point
 
 
 class OperatorOracle(BaseOracle):
-    def __init__(self, C: torch.Tensor, q: torch.Tensor, n: int, full: bool = True):
+    def __init__(self, C: torch.Tensor, q: torch.Tensor, n: int, full: bool = True, gamma: float = 0.001):
         """
         :param torch.Tensor C: Cost matrix of shape (d, d).
         :param torch.Tensor q: Marginal distributions of shape (T, d).
@@ -17,28 +17,31 @@ class OperatorOracle(BaseOracle):
         self._C_norm = torch.max(self._C)
         self._q = q
         self._full = full
+        self._gamma = gamma
         assert self._n <= self._T
         assert C.shape == (self._d, self._d)
 
     def f(self, z: Point) -> float:
         x, p, u, v, q, denominator = self._preprocess_z(z)
 
-        result = torch.sum(self._C[None, :, :] * x) + 2 * self._C_norm * torch.sum(
-            u * (x.sum(dim=2) - p[None, :]) + v * (x.sum(dim=1) - q)
+        result = (
+            torch.sum(self._C[None, :, :] * x)
+            + 2 * self._C_norm * torch.sum(u * (x.sum(dim=2) - p[None, :]) + v * (x.sum(dim=1) - q))
+            + 0.5 * self._gamma * (torch.sum(p * p) + torch.sum(x * x))
         )
 
         return result / denominator
 
     def grad_x(self, z: Point) -> torch.Tensor:
-        _, _, u, v, _, denominator = self._preprocess_z(z)
-        grad = self._C.repeat(denominator, 1, 1) + 2 * self._C_norm * (u[:, :, None] + v[:, None, :])
+        x, _, u, v, _, denominator = self._preprocess_z(z)
+        grad = self._C.repeat(denominator, 1, 1) + 2 * self._C_norm * (u[:, :, None] + v[:, None, :]) + self._gamma * x
 
         return torch.cat((grad, torch.zeros((self._T - denominator, self._d, self._d)))) / denominator
 
     def grad_p(self, z: Point) -> torch.Tensor:
-        _, _, u, _, _, denominator = self._preprocess_z(z)
+        _, p, u, _, _, denominator = self._preprocess_z(z)
 
-        return -u.sum(dim=0) / denominator
+        return (-u.sum(dim=0) + self._gamma * p) / denominator
 
     def grad_u(self, z: Point) -> torch.Tensor:
         x, p, _, _, _, denominator = self._preprocess_z(z)
